9696906ed8f9385ba0f711ae5ca7db84569c9755
D3m0nK1113r <d3m0nk1113r99@gmail.com>
2023-02-03 13:25:32 -0500

    Fixed PartTimeStudent class

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,5 +1,8 @@
 package exercise1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
@@ -13,16 +16,17 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] hand = new Card[7];
-
+        Random random = new Random();
+        Scanner scan = new Scanner(System.in);
+        
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
+            card.setValue((int) ((Math.random() * (13 - 1)) + 1));
             // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            card.setSuit(Card.SUITS[random.nextInt(4)]);
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
         }
-
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
         // Hint: You can ask for values 1 to 10, and then
@@ -30,9 +34,27 @@ public class CardTrick {
         //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
         // 
         // Then loop through the cards in the array to see if there's a match.
-        
         // If the guess is successful, invoke the printInfo() method below.
         
+        Card card2 = new Card();
+        while (true) {
+            System.out.println("Enter the card value: ");           
+            int value = scan.nextInt();
+            System.out.println("Enter the card suit: ");
+            String suit = scan.next();
+            if (value == card2.getValue() && suit.equals(card2.getSuit())) {
+                printInfo();
+                break;
+            } else if (value == card2.getValue() && suit.equals(card2.getSuit())) {
+                System.out.println("The value is correct, but the suit is incorrect.");
+            } else if (value != card2.getValue() && suit.equals(card2.getSuit())) {
+                System.out.println("The value is incorrect, but the suit is "
+                        + ""
+                        + "correct.");
+            } else {
+                System.out.println("Both the value and the suit are incorrect.");
+            }
+        }
     }
 
     /**
